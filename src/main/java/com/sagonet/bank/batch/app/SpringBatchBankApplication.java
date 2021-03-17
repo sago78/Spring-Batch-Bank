4fd279962f4119c63a551d04f3c068e7b293f579
vvb34579 <Julio.SanchezGonzalez@igt.com>
2021-03-17 12:42:17 -0600

    initial commit

---
+++ src/main/java/com/sagonet/bank/batch/app/SpringBatchBankApplication.java
@@ -0,0 +1,13 @@
+package com.sagonet.bank.batch.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sagonet/bank/batch/app/SpringBatchBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagonet.bank.batch.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
